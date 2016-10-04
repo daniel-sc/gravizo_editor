db81a4199d3b312b100ebfabc5266c94070e3e7a
Daniel Schreiber <daniel-schreiber@gmx.de>
2016-10-04 17:37:42 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, FormControl }   from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { routing,
          appRoutingProviders }  from './app.routing';
@@ -8,6 +10,8 @@ import { Graph }   from './graph';
 @NgModule({
   imports: [ 
     BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule,
     routing 
   ],
   declarations: [ Graph ],
