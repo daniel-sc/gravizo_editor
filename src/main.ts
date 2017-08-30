43621553325698bac7235352a37c21caa7cd3fcb
Daniel Schreiber <daniel-schreiber@gmx.de>
2017-08-31 00:16:19 +0200

    Auto-generated commit

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-//main entry point
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
